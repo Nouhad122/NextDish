ad51549ed8300be5f058b93854fc08790f180d43
Nouhad122 <nouhadalhallab122@gmail.com>
2025-02-03 13:16:25 +0300

    posted meal in the database

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,6 @@
 import ImagePicker from '@/Components/ImagePicker/ImagePicker';
 import classes from './page.module.css';
-import { handleSubmission } from '@/lib/action';
+import { shareMeal } from '@/lib/action';
 
 const ShareMealPage = () => {
   return (
@@ -12,7 +12,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={handleSubmission}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
